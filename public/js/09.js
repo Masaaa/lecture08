ce041bbf477ee6f3c190753cba8adb09ac410d73
Masaaa <keio.sfc.masa@gmail.com>
2015-06-04 12:18:53 +0900

    中間試験追加

--- public/js/09.js
+++ public/js/09.js
@@ -1,25 +1,3 @@
-function zero(){
-  return [];
-}
-
-
-function succ(a){
-  a.push(true)
-  return a;
-}
-
-
-
-function n(b){
-  if(b < 0){
-    return null
-  }
-  var a = zero();
-  var i = 0;
-
-    while(a.length < b){
-    a = succ(a);
-  }
-
-  return a;
+function projection(n){
+ return n.name;
 }
